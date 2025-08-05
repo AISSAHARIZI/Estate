d0209e3c556df4b5d89eeca032c1b38e2e867ac2
AISSAHARIZI <your_AISSAHARIZI_email@example.com>
2025-08-05 11:08:29 +0100

    cleaned the cod

--- src/app/component/Project.jsx
+++ src/app/component/Project.jsx
@@ -1,25 +1,18 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { projectsData } from "../../../public/assets";
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
+
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { Card, CardContent } from "@/components/ui/card"
-function Project() {
-  const [curentIndex, setCurentindex] = useState(0);
 
-  function next() {
-    setCurentindex((prev) => (prev + 1) % (projectsData.length - 1));
-  }
-  function preves() {
-    setCurentindex((prev) =>
-      prev === 0 ? projectsData.length - 1 : (prev - 1) % projectsData.length
-    );
-  }
+function Project() {
+  
+
+ 
   return (
     <div className="my-50 space-y-15 container items-center w-full overflow-hidden mx-auto">
       <div className="flex flex-col items-center gap-4">
@@ -62,17 +55,7 @@ function Project() {
     </Carousel>
         
       </div>
-      <div className="overflow-hidden">
-        <div className="flex  justify-end items-center gap-2  mb-5">
-          <button onClick={preves} className="bg-gray-200  rounded-sm p-3">
-            <img src={"left_arrow.svg"} alt="" />
-          </button>
-          <button onClick={next} className="bg-gray-200 rounded-sm p-3">
-            <img src={"right_arrow.svg"} alt="" />
-          </button>
-        </div>
-        
-      </div>
+      
     </div>
   );
 }
